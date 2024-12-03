ac22b0fc4cdb9f7286f3603454ed5d2eecdd69e0
WwyDev <wwwwwydev@gmail.com>
2024-12-03 15:28:05 +0800

    init

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/go-needle/orm
 go 1.21
 
 require github.com/mattn/go-sqlite3 v1.14.24
+
+require github.com/go-needle/log v0.0.0-20241203071043-d4021d76d384
---
+++ orm_test.go
@@ -0,0 +1,19 @@
+package orm
+
+import (
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"testing"
+)
+
+func TestOrm(t *testing.T) {
+	engine, _ := NewEngine("sqlite3", "gee.db")
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text);").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text);").Exec()
+	result, _ := s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Tom1", "Sam").Exec()
+	count, _ := result.RowsAffected()
+	fmt.Printf("Exec success, %d affected\n", count)
+}
