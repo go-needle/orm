240a89176f6d9891098cff6a0362a681a95eb157
WwyDev <wwwwwydev@gmail.com>
2024-12-10 16:49:22 +0800

    add Migrate

--- orm.go
+++ orm.go
@@ -2,9 +2,11 @@ package orm
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/go-needle/orm/dialect"
 	"github.com/go-needle/orm/log"
 	"github.com/go-needle/orm/session"
+	"strings"
 )
 
 type Engine struct {
@@ -64,4 +66,54 @@ func (engine *Engine) Transaction(f TxFunc) (result any, err error) {
 	}()
 
 	return f(s)
+}
+
+// difference returns a - b
+func difference(a []string, b []string) (diff []string) {
+	setB := make(map[string]struct{})
+	for _, v := range b {
+		setB[v] = struct{}{}
+	}
+	for _, v := range a {
+		if _, ok := setB[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+	return
+}
+
+// Migrate table
+func (engine *Engine) Migrate(value any) error {
+	_, err := engine.Transaction(func(s *session.Session) (result any, err error) {
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesn't exist", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+		table := s.RefTable()
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1", table.Name)).QueryRows()
+		columns, _ := rows.Columns()
+		addCols := difference(table.MappingFieldNames, columns)
+		delCols := difference(columns, table.MappingFieldNames)
+		log.Infof("added cols %v, deleted cols %v", addCols, delCols)
+
+		for _, col := range addCols {
+			f := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s", table.Name, f.MappingName, f.Type)
+			if _, err = s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+
+		if len(delCols) == 0 {
+			return
+		}
+		tmp := "tmp_" + table.Name
+		fieldStr := strings.Join(table.MappingFieldNames, ", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s from %s;", tmp, fieldStr, table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s;", tmp, table.Name))
+		_, err = s.Exec()
+		return
+	})
+	return err
 }
